76fa9381374f6a11b203431dd15b44e69a494189
Vlad Botar <vlad.botar99@gmail.com>
2024-10-05 22:04:03 +0300

    added firebase integration + recipes endpoints

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private dataStorageSevice: DataStorageService) {}
+
+  onFetchData() {
+    this.dataStorageSevice.fetchRecipes().subscribe();
+  }
+
+  onSaveData() {
+    this.dataStorageSevice.storeRecipes();
+  }
+}
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,24 +7,30 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      1,
-      'ciorbica',
-      'o reteta cu ciorbica',
-      'https://savoriurbane.com/wp-content/uploads/2018/01/Ciorba-de-potroace-reteta-traditionala-savori-urbane.jpg',
-      [new Ingredient('apa', 1), new Ingredient('carne de porcenstein', 2)]
-    ),
-    new Recipe(
-      2,
-      'strudel',
-      'strudel ca la Anaconda',
-      'https://www.culinar.ro/uploads/modules/news/0/2015/9/23/8411/fdb_1444083698_lead_800x600_8411.jpg',
-      [new Ingredient('aluat', 1), new Ingredient('mar', 3)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     1,
+  //     'ciorbica',
+  //     'o reteta cu ciorbica',
+  //     'https://savoriurbane.com/wp-content/uploads/2018/01/Ciorba-de-potroace-reteta-traditionala-savori-urbane.jpg',
+  //     [new Ingredient('apa', 1), new Ingredient('carne de porcenstein', 2)]
+  //   ),
+  //   new Recipe(
+  //     2,
+  //     'strudel',
+  //     'strudel ca la Anaconda',
+  //     'https://www.culinar.ro/uploads/modules/news/0/2015/9/23/8411/fdb_1444083698_lead_800x600_8411.jpg',
+  //     [new Ingredient('aluat', 1), new Ingredient('mar', 3)]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next([...this.recipes]);
+  }
 
   getNextId(): number {
     return this.recipes.length + 1;
