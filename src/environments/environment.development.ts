25dad0a061a83a6c12e0a128a61e9feedbfe6a1b
Vlad Botar <vlad.botar99@gmail.com>
2024-10-23 14:59:06 +0300

    Removed env files from tracking

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
-export const environment = {
-  production: false,
-  firebaseApiKey: 'AIzaSyAy9187JjB_RoZB-QWrg26RWqUwLfyMYY4',
-  imports: [StoreDevtoolsModule.instrument({ maxAge: 25 })],
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  firebaseApiKey: 'AIzaSyAy9187JjB_RoZB-QWrg26RWqUwLfyMYY4',
-  imports: [],
-};
